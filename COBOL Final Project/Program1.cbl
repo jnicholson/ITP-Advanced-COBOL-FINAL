9ac39d9bef8ed1d99bb73de084d23840b7541781
jnicholson <ja.nicholso@gmail.com>
2014-03-16 14:48:07 -0500

    Create Test Files

---
+++ COBOL Final Project/Program1.cbl
@@ -0,0 +1,14 @@
+       program-id. Program1 as "Program1".
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+
+
+           goback.
+
+       end program Program1.
