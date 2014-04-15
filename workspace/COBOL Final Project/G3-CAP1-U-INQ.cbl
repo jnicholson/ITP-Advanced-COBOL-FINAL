a7206cf86cdb535d36d8802260f1318f8948856f
rttimmerman <ryantoddt@hotmail.com>
2014-04-15 10:45:50 -0500

    Added Cap1 User Inq files

---
+++ workspace/COBOL Final Project/G3-CAP1-U-INQ.cbl
@@ -0,0 +1,18 @@
+      *******************************************************************                                                                                            
+      *PROGRAM:  Capital One User Inquiry                               *                                                                                            
+      *AUTHOR:   Ryan Timmerman                                         *                                                                                            
+      *DATE:     4/15/2014                                              *                                                                                            
+      *ABSTRACT: User Inquiry                                           *                                                                                            
+      *******************************************************************                                                                                            
+       IDENTIFICATION DIVISION.                                                                                                                                      
+       PROGRAM-ID. G3-CAP1-U-INQ AS "G3-CAP1-U-INQ".                                                                                   
+      ******************************************************************                                                                                             
+       ENVIRONMENT DIVISION.                                                                                                                                         
+      ******************************************************************                                                                                             
+       DATA DIVISION.                                                                                                                                                
+       WORKING-STORAGE SECTION.                                                                                                                                      
+       COPY SCREEN-CAP1-PAY.                                                                                                                       
+      ******************************************************************                                                                                             
+       PROCEDURE DIVISION.                                                                                                                                           
+       000-MAIN.                                                                                                                                                     
+       DISPLAY MAIN-SCREEN.                                                                                                                                
